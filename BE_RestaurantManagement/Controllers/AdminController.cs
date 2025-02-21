f239634725d2f7c72967b4a829663e8f1e1ec1c6
hungtabe <trandinhhung717@gmail.com>
2025-02-21 13:35:48 +0700

    [rm_8] Develop update user's role by Admin

--- BE_RestaurantManagement/Controllers/AdminController.cs
+++ BE_RestaurantManagement/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using BE_RestaurantManagement.DTOs;
 using BE_RestaurantManagement.Interfaces;
+using BE_RestaurantManagement.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +26,18 @@ namespace BE_RestaurantManagement.Controllers
                 return NotFound(new { message = "User not exist!" });
 
             return Ok(new { message = "Reset password successfully!" });
+        }
+
+        [HttpPut("change-role")]
+        public async Task<IActionResult> ChangeUserRole(int userId, int newRoleId)
+        {
+            var result = await _adminService.ChangeUserRoleAsync(userId, newRoleId);
+            if (!result)
+            {
+                return NotFound(new { message = "User not found or role update failed" });
+            }
+
+            return Ok(new { message = "User role updated successfully" });
         }
     }
 }
